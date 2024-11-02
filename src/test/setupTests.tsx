cdec692e425b7618494d3abaf54e51099f5a4b13
Lucas Rodrigues de Souza <Rodriguesdesouzal28@gmail.com>
2024-11-02 18:39:10 -0300

    feat: update API base URL e setup test

--- src/test/setupTests.tsx
+++ src/test/setupTests.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter as Router } from "react-router-dom";
 import { render } from "@testing-library/react";
+import { Toaster } from "@/components/ui/toaster";
 const queryClient = new QueryClient();
 
 const AppProvider = ({ children }: { children: ReactNode }) => {
@@ -11,6 +12,7 @@ const AppProvider = ({ children }: { children: ReactNode }) => {
       <AuthProvider>
         <QueryClientProvider client={queryClient}>
           {children}
+          <Toaster />
         </QueryClientProvider>
       </AuthProvider>
     </Router>
